1651cd95a1f3336f9cf32c9f22dfb89346c44917
Jacob Larson <hockeybobber@comcast.net>
2023-11-30 13:37:06 -0600

    made the add button work

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -1,6 +1,17 @@
-
+import { useSelector, useDispatch } from 'react-redux'
 
 function PizzaItem({ pizza }) {
+    const dispatch = useDispatch()
+
+
+    const addButton = () => {
+        dispatch({
+            type: 'ADD_PIZZA_TO_CART',
+            payload: pizza
+          })
+    }
+    
+    
     
     return (
         <ul>
@@ -8,6 +19,8 @@ function PizzaItem({ pizza }) {
             <li>{pizza.description}</li>
             <li>{pizza.price}</li>
             <li>{pizza.image_path}</li>
+            <button onClick={addButton}>Add</button>
+            <button>Remove</button>
         </ul>
     )
     
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,16 +4,23 @@ import logger from 'redux-logger';
 // Be sure to replace this reducer! 🙂
 const pizzaMenu = (state = [], action) => {
   if (action.type === 'SET_PIZZA') {
-      
     return action.payload
 }
-  
   return state;
 }
 
+const addPizzaToCart = (state = [], action) => {
+  if (action.type === 'ADD_PIZZA_TO_CART') {
+    return [...state, action.payload]
+}
+  return state;
+}
+
+
 const store = createStore(
   combineReducers({
-    pizzaMenu
+    pizzaMenu,
+    addPizzaToCart
   }),
   applyMiddleware(logger),
 );
